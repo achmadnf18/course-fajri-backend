8a0bb66b95b36326e18f1e8c662583969543d32d
fajri1 <achmad17nov@gmail.com>
2021-09-19 07:16:21 +0700

    prepare for deploy

--- src/index.ts
+++ src/index.ts
@@ -40,7 +40,7 @@ class App {
     }
 }
 
-const port: Number = 3010;
+const port = process.env.PORT || 3010;
 const { app } = new App();
 app.listen(port, () => {
   console.log(`listen on port ${port}`);
