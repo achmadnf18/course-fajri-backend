2821342fd2e2d1d2557f9bc31ae7e5818847f42b
fajri1 <achmad17nov@gmail.com>
2021-09-19 09:15:46 +0700

    update: database.js for production

--- src/config/database.js
+++ src/config/database.js
@@ -19,10 +19,11 @@ module.exports = {
     dialect: 'mysql'
   },
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DATABASE_URL,
+    host: config.host,
+    port: config.port,
+    database: config.database,
+    username: config.user,
+    password: config.password,
     dialect: 'postgresql'
   }
 };
